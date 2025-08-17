73222ae45c5fe4aa5b6e363cb6ca3cd5ab5b32eb
Aleksey Devyatkin <>
2025-08-17 23:19:01 +0300

    [#51] Added ty option

---
+++ sample-projects/poetry-project-mypy-wemake-black/poetry_project_mypy_wemake_black/__init__.py
@@ -0,0 +1,14 @@
+# mypy: disable-error-code="attr-defined"
+"""Awesome `poetry-project-mypy-wemake-black` project!"""
+
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+__version__: str = get_version()
